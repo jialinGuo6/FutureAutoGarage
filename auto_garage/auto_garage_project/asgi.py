86a0ddc27cb51a462b19bc05fcbcea51fb1a192c
jialinGuo6 <jguo6@unb.ca>
2025-11-26 23:52:01 -0400

    你的提交说明，比如修复了 bug 或新增功能

--- auto_garage/auto_garage_project/asgi.py
+++ auto_garage/auto_garage_project/asgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auto_garage_project.settings')
-
+# Auto choose the cofiguration based on .env file: dev or prod 
+env = os.environ.get("DJANGO_ENV", "dev")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'auto_garage_project.settings.{env}')
 application = get_asgi_application()
--- auto_garage/auto_garage_project/wsgi.py
+++ auto_garage/auto_garage_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auto_garage_project.settings')
-
+env = os.environ.get("DJANGO_ENV", "dev")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'auto_garage_project.settings.{env}')
 application = get_wsgi_application()
