6c188ca349c416902045ee78ddbd80edcf99199f
jialinGuo6 <jguo6@unb.ca>
2025-11-26 23:47:12 -0400

    Refactor settings to package and update frontend build

---
+++ auto_garage/auto_garage_project/settings/dev.py
@@ -0,0 +1,22 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
---
+++ auto_garage/auto_garage_project/settings/prod.py
@@ -0,0 +1,35 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS_STR = os.environ.get('DJANGO_ALLOWED_HOSTS', '')
+ALLOWED_HOSTS = ALLOWED_HOSTS_STR.split(',') if ALLOWED_HOSTS_STR else []
+
+# SQLite 虽然可用，但生产建议换 MySQL/PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+CORS_ALLOWED_ORIGINS = [
+    "https://futureautogarage.com",
+    "https://www.futureautogarage.com",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://futureautogarage.com",
+    "https://www.futureautogarage.com",
+]
+
+# 强制 HTTPS
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
